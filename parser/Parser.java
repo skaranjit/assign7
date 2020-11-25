b4bb5d64eeb6bc7cc5645bd41ad86d4f7ec9d835
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-11-25 12:13:37 -0600

    Update Parser.java

--- parser/Parser.java
+++ parser/Parser.java
@@ -719,7 +719,7 @@ public class Parser extends ASTVisitor
         if (look.tag == Tag.ID)
         {
             rhs_assign = new IdentifierNode();
-	    (IdentifierNode)rhs_assign.w = look;
+	    ((IdentifierNode)rhs_assign).w = look;
 	    System.out.println("Inside Parenthesis Identifier node");
 	    
             ((IdentifierNode)rhs_assign).accept(this);
