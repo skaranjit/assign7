7d9a871f06e67052bca012305f35d3a8d60c7176
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-11-25 12:19:10 -0600

    Update Parser.java

--- parser/Parser.java
+++ parser/Parser.java
@@ -648,7 +648,7 @@ public class Parser extends ASTVisitor
         n.id = look.toString();
         n.w = (Word)look; // new code
 	if((IdentifierNode)top.get(n.w) != null){
-		n = (IdentifierNode)top.get((Word)look);
+		n.type = (IdentifierNode)top.get((Word)look).type;
 		println(n.id + " Identifier from top");
 	}
 	
