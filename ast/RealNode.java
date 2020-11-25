62c6ed5d0dcf5a01a448267ae5b2397f2d90cf05
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-11-25 11:32:22 -0600

    Update RealNode.java

--- ast/RealNode.java
+++ ast/RealNode.java
@@ -1,7 +1,7 @@
-package assign6.ast;
+package assign7.ast;
 
-import assign6.visitor.*;
-import assign6.lexer.*;
+import assign7.visitor.*;
+import assign7.lexer.*;
 
 public class RealNode extends ExprNode
 {
