8633acad110a2971c9acf551ef843856a2d54264
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-11-25 13:54:24 -0600

    Update Parser.java

--- parser/Parser.java
+++ parser/Parser.java
@@ -250,8 +250,6 @@ public class Parser extends ASTVisitor
         n.left = new IdentifierNode();
         n.left.accept(this);
 
-       // IdentifierNode id = (IdentifierNode)top.get(((IdentifierNode)n.left).w); // new code
-        println("In Parser, AssignmentNode's left type: " + id.type); // new code
 
         match('=');
         ExprNode rhs_assign = null;
