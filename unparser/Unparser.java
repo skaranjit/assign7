74350a777e7f81a50b6cca5d3776d1fb9e0d2369
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-11-25 16:17:04 -0600

    Update Unparser.java

--- unparser/Unparser.java
+++ unparser/Unparser.java
@@ -142,8 +142,9 @@ public class Unparser extends ASTVisitor
     public void visit(ArrayTypeNode n)
     {
         print("[");
-        if(n.size instanceof IdentifierNode) print("" + ((IdentifierNode)n.size).w);
-        if(n.size instanceof NumNode) print("" + ((NumNode)n.size).value);
+        ((BinnExprNode)n.size).accept(this);
+//         if(n.size instanceof IdentifierNode) print("" + ((IdentifierNode)n.size).w);
+//         if(n.size instanceof NumNode) print("" + ((NumNode)n.size).value);
 //         print("" +n.size);
         print("]");
 
@@ -162,6 +163,20 @@ public class Unparser extends ASTVisitor
 
     public void visit(StatementNode n)
     {
+        if(n.stmt instanceof ConditionalNode)
+            ((ConditionalNode)n.stmt).accept(this);
+
+        else if(n.stmt instanceof WhileNode)
+            ((WhileNode)n.stmt).accept(this);
+
+        else if(n.stmt instanceof DoWhileNode)
+            ((DoWhileNode)n.stmt).accept(this);
+        else if(n.stmt instanceof DoWhileNode)
+            ((DoWhileNode)n.stmt).accept(this);
+        else if(n.stmt instanceof BlockStatementNode)
+            ((BlockStatementNode)n.stmt).accept(this);
+        else if(n.stmt instanceof AssignmentNode)
+            ((AssignmentNode)n.stmt).accept(this);
 
     }
 
