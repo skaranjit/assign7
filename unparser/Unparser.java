3bccbfe9de7d8eb1feee384d49a14741b25874bf
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-12-06 04:12:35 -0600

    Update Unparser.java

--- unparser/Unparser.java
+++ unparser/Unparser.java
@@ -170,16 +170,7 @@ public class Unparser extends ASTVisitor
 
     public void visit(BinExprNode n)
     {
-    	if(!n.assigns.isEmpty()){
-		println("Debug:");
-		bR = n.left;
-		Count++;
-		n.left = a;
-		println("Test a:");
-		a.printNode();
-		temp1 = n.right;
-		n.right = bR;
-	}
+    	
         if (n.left instanceof IdentifierNode)
         {
             ((IdentifierNode)n.left).accept(this);
