f175eb7678f9c30fa35abbebb6c75dc1e2e58568
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-12-07 13:40:07 -0600

    Update ArrayTypeNode.java

--- ast/ArrayTypeNode.java
+++ ast/ArrayTypeNode.java
@@ -2,8 +2,11 @@ package assign7.ast;
 
 import assign7.visitor.*;
 import assign7.lexer.*;
+import assign7.ast.*;
+import java.util.*;
 public class ArrayTypeNode extends TypeNode
 {
+    public List<AssignmentNode> assigns = new ArrayList<AssignmentNode>();
     public TypeNode type;
     public ExprNode size;
 
