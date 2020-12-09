8f2fca5683d36a312f896b1054359bfac1aecf05
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-12-09 11:39:11 -0600

    Update BinExprNode.java

--- ast/BinExprNode.java
+++ ast/BinExprNode.java
@@ -11,14 +11,7 @@ public class BinExprNode extends ExprNode
     public ExprNode right;
     public Token op;
     public Type type;
-     //AssignmentNode which will be added before IFStatementNode for Intermediate Code
-    
-    public List<AssignmentNode> assigns = new ArrayList<AssignmentNode>();
-    
-    
-    //Label for intermediate Code
-    public LabelNode falseLabel;
-    
+ 
     public BinExprNode()
     {
 
