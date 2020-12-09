a91709bf0867edfe139081a247042f97d4a24e5c
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-12-09 11:37:15 -0600

    Update StatementNode.java

--- ast/StatementNode.java
+++ ast/StatementNode.java
@@ -5,6 +5,15 @@ import assign7.visitor.ASTVisitor;
 public class StatementNode
 {
     public StatementNode stmt;
+     //AssignmentNode which will be added before IFStatementNode for Intermediate Code
+    
+    public List<AssignmentNode> assigns = new ArrayList<AssignmentNode>();
+    
+    
+    //Label for intermediate Code
+    public LabelNode falseLabel;
+    public LabelNode startLabel;
+    public GotoNode toGoto;
     public void accept(ASTVisitor v)
     {
         v.visit(this);
